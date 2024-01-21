884394d0583a3586e949f387ff8e391bb7b5598a
anya-sem <139631738+anya-sem@users.noreply.github.com>
2024-01-21 15:35:08 +0200

    corrections 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-// import './App.css'
 
 import { Description } from './Description/Description'
 import { Options } from './Options/Options'
@@ -21,29 +20,18 @@ function App() {
     }
   });
   
-  const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks");
-
-    if (savedClicks !== null) {
-      return JSON.parse(savedClicks);
-    }
-    return 0;
-  });
-  
 
   const onFeedback = (option) => {
     setValues({
       ...values,
       [option]: values[option] + 1
     });
-    
-    setClicks(clicks + 1);
   };
 
   const totalFeedback = values.good + values.neutral + values.bad;
   const positiveFeedback = Math.round(((values.good + values.neutral) / totalFeedback) * 100);
   
-  const isHidden = clicks === 0;
+  const isHidden = totalFeedback === 0;
 
   const onReset = () => {
     setValues({
@@ -52,13 +40,11 @@ function App() {
       neutral: 0,
       bad: 0
     });
-    setClicks(0)
   };
 
   useEffect(() => {
     window.localStorage.setItem("saved-values", JSON.stringify(values));
-    window.localStorage.setItem("saved-clicks", clicks);
-  }, [values, clicks]);
+  }, [values]);
 
 
   return (
